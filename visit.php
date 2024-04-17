86aafc138842b7559699a72994e9c885da878e6b
Praise Wenegieme <praisewenegieme29@gmail.com>
2024-04-17 14:27:37 +0100

    project complete

--- visit.php
+++ visit.php
@@ -4,12 +4,21 @@ if(isset($_POST['name'])) {
     // Retrieve the submitted name
     $name = $_POST['name'];
 
-    // Store the visit time and name in a cookie
-    setcookie("visitor_name", $name, time() + 3600, "/");
-    setcookie("visit_time", time(), time() + 3600, "/");
+    // Check if the opt-out checkbox is checked
+    $optOut = isset($_POST['opt-out']) ? true : false;
 
-    // Redirect back to the page after storing the name
-    header("Location: index.html");
+    // Store the visit time and name in cookies only if user has not opted out
+    if (!$optOut) {
+        setcookie("visitor_name", $name, time() + 3600, "/");
+        setcookie("visit_time", time(), time() + 3600, "/");
+    }
+
+    // Redirect to a different page based on user's decision
+    if ($optOut) {
+        header("Location: main.html");
+    } else {
+        header("Location: main.html");
+    }
     exit();
 } else {
     echo "Name not submitted!";
