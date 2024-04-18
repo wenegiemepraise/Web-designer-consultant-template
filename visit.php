83f9e446c3265f2b3d879629c615c6d8a0cbe28b
Praise Wenegieme <praisewenegieme29@gmail.com>
2024-04-18 15:43:57 +0100

    Final final version

--- visit.php
+++ visit.php
@@ -7,13 +7,17 @@ if(isset($_POST['name'])) {
     // Check if the opt-out checkbox is checked
     $optOut = isset($_POST['opt-out']) ? true : false;
 
+    // Set expiration time for the cookie
+    $expiration = time() + (365 * 24 * 60 * 60); // 1 year
+
     // Store the visit time and name in cookies only if user has not opted out
     if (!$optOut) {
-        setcookie("visitor_name", $name, time() + 3600, "/");
-        setcookie("visit_time", time(), time() + 3600, "/");
+        // Set cookies
+        setcookie("visitor_name", $name, $expiration, "/");
+        setcookie("visit_time", time(), $expiration, "/");
     }
 
-    //Redirect to webpage.
+    // Redirect to webpage.
     header("Location: main.html");
     
     exit();
